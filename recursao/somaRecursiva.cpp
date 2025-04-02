112cc98f3b318ecc3cedfc55aeb7c571f86a1cc3
Wallyson Matheus <37665843+wallysom2@users.noreply.github.com>
2025-04-02 18:17:42 +0000

    soma recursiva resolucao

--- recursao/somaRecursiva.cpp
+++ recursao/somaRecursiva.cpp
@@ -7,24 +7,34 @@
  * Este código é licenciado sob a Licença MIT.
  */
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "../doctest.h"
-
-/**
- * @brief Soma dois números inteiros X e Y usando adições sucessivas e recursão.
- *
- * @param X O primeiro número a ser somado.
- * @param Y O segundo número a ser somado.
- * @return O resultado da soma de X e Y.
- */
-int soma(int X, int Y) {
-    return -1;
-}
-
-TEST_CASE("Somar Dois Números Inteiros - Testes") {
-    CHECK(soma(3, 4) == 7);
-    CHECK(soma(0, 5) == 5);
-    CHECK(soma(5, 0) == 5);
-    CHECK(soma(3, 6) == 9);
-    CHECK(soma(7, 3) == 10);
-}
+ #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+ #include "../doctest.h"
+ 
+ /**
+  * @brief Soma dois números inteiros X e Y usando adições sucessivas e recursão.
+  *
+  * @param X O primeiro número a ser somado.
+  * @param Y O segundo número a ser somado.
+  * @return O resultado da soma de X e Y.
+  */
+ int soma(int X, int Y) {
+     if (Y == 0){
+         return X;
+     }
+     if (Y > 0){
+         return soma (X + 1, Y - 1);
+     }
+     if (Y < 0){
+         return soma (X - 1, Y + 1);
+     }
+     return 0;
+ }
+ 
+ TEST_CASE("Somar Dois Números Inteiros - Testes") {
+     CHECK(soma(3, 4) == 7);
+     CHECK(soma(0, 5) == 5);
+     CHECK(soma(5, 0) == 5);
+     CHECK(soma(3, 6) == 9);
+     CHECK(soma(7, 3) == 10);
+ }
+ 
